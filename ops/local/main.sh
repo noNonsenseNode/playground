a93ed750ab149741bf7e5bded77bc094eb8fa0be
ghafran <ghafranabbas@gmail.com>
2016-05-27 09:36:00 -0400

    Setup Docker Container Build

---
+++ ops/local/main.sh
@@ -0,0 +1,1 @@
+sed 's/\r$//' /srv/ops/local/start.sh > /srv/ops/local/start2.sh; bash /srv/ops/local/start2.sh;
---
+++ ops/local/start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+node-inspector --no-preload --web-port=8080 --save-live-edit=true --hidden=node_modules &
+
+forever -a --uid="playground" -c "node --debug=4001" /srv/server.js &
+
+while true; do sleep 10000; done
